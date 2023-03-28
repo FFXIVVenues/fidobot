980e2112caa151353b15264ae499a4db149cfb6e
Exorion1er <jason.rivillon@gmail.com>
2023-03-28 18:54:54 +0200

    Extension method to get the forum parent of a thread

--- Utilities/DiscordHelper.cs
+++ Utilities/DiscordHelper.cs
@@ -6,6 +6,19 @@ namespace Fidobot.Utilities;
 public static class DiscordHelper {
   public static readonly DiscordSocketClient client = new();
 
+  public static IForumChannel GetParentForum(this IThreadChannel thread) {
+    if (thread == null)
+      throw new ArgumentNullException(nameof(thread));
+
+    if (thread is not SocketThreadChannel socketThread)
+      throw new ArgumentException("The channel is not a SocketThreadChannel.", nameof(thread));
+
+    if (socketThread.ParentChannel is not SocketGuildChannel socketForum)
+      throw new ArgumentException("The parent channel is not a SocketGuildChannel.", nameof(thread));
+
+    return socketForum as IForumChannel ?? throw new ArgumentException("The parent channel is not a forum channel.", nameof(thread));
+  }
+
   public static async Task<IGuildChannel?> GetChannel(ulong guildID, ulong channelID) {
     IGuild guild = client.GetGuild(guildID);
     if (guild == null) {
